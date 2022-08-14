9d0fa712d6c836780103a9cd873aecfd04d31a3f
wrickard <wrickard@student.21-school.ru>
2022-08-14 20:50:35 +0300

    console input with database

--- src/main/java/electricity/meter/app/Program.java
+++ src/main/java/electricity/meter/app/Program.java
@@ -20,7 +20,7 @@ public class Program {
         Scanner scan = new Scanner(System.in);
         System.out.println("Введите код плательщика: ");
         Long id = scan.nextLong();
-        System.out.println("Введите период показаний[MMYYYY]: ");
+        System.out.println("Введите период показаний [MMYYYY]: ");
         Integer period = scan.nextInt();
         System.out.println("Введите показания счетчика [кВт/ч]: ");
         Double data = scan.nextDouble();
